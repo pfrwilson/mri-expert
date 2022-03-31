f086498b8f7c51edfbd0e8cb5e6eda7751829d4c
Paul Wilson <pfrwilson@gmail.com>
2022-03-31 17:10:19 -0400

    changed default crop to 224

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,7 +3,7 @@ from .dataset import PCASegmentationDataset
 from .preprocess import Preprocessor
 from torch.utils.data import DataLoader
 
-def train_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=256, 
+def train_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=224, 
                      use_augmentations=True, **preprocessor_kwargs):
     
     preprocess = Preprocessor(target_size=target_size, use_augmentations=use_augmentations, 
@@ -19,7 +19,7 @@ def train_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=256
     
     return DataLoader(dataset, batch_size, shuffle=True)
 
-def val_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=256, 
+def val_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=224, 
                    **preprocessor_kwargs):
     
     preprocess = Preprocessor(target_size=target_size, use_augmentations=False,
