7af726e0156b0eab8cb0f3a56630783e05aa17c7
Paul Wilson <pfrwilson@gmail.com>
2022-04-01 14:36:41 -0400

    return train, val dataloaders with single function

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,32 +3,32 @@ from .dataset import PCASegmentationDataset
 from .preprocess import Preprocessor
 from torch.utils.data import DataLoader
 
-def train_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=224, 
+def dataloaders(root, batch_size, split_seed=0, val_size=5, target_size=224, 
                      use_augmentations=True, **preprocessor_kwargs):
     
-    preprocess = Preprocessor(target_size=target_size, use_augmentations=use_augmentations, 
+    train_preprocess = Preprocessor(target_size=target_size, use_augmentations=use_augmentations, 
                               **preprocessor_kwargs)
     
-    dataset = PCASegmentationDataset(
+    val_preprocess = Preprocessor(target_size=target_size, use_augmentations=False, 
+                              **preprocessor_kwargs)
+    
+    train = PCASegmentationDataset(
         root, 
         'train', 
-        preprocess=preprocess, 
+        preprocess=train_preprocess, 
         split_seed=split_seed, 
         val_size=val_size,
     )
     
-    return DataLoader(dataset, batch_size, shuffle=True)
-
-def val_dataloader(root, batch_size, split_seed=0, val_size=5, target_size=224, 
-                   **preprocessor_kwargs):
-    
-    preprocess = Preprocessor(target_size=target_size, use_augmentations=False,
-                              **preprocessor_kwargs)
-    
-    dataset = PCASegmentationDataset(
-        root, 'val', 
-        preprocess=preprocess, 
-        split_seed=split_seed,
+    val = PCASegmentationDataset(
+        root,
+        'val', 
+        preprocess=val_preprocess,
+        split_seed=split_seed, 
         val_size=val_size
     )
     
+    return (DataLoader(train, batch_size, shuffle=True), 
+                DataLoader(val, batch_size, shuffle=False))
+
+
