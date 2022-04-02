f591935fb71c7288dd0168e8d227798259a93d61
Paul Wilson <pfrwilson@gmail.com>
2022-04-02 14:06:03 -0400

    feature: log images with masks to wandb

--- src/logging.py
+++ src/logging.py
@@ -21,53 +21,46 @@ def wb_mask(bg_img, pred_mask, true_mask):
         "ground truth" : {"mask_data" : true_mask, "class_labels" : labels()}})
   
 
-class LogMasksCallback: 
+def log_images_with_masks(name, model, dataloader, num_images, choose_randomly, device):
+
+    ds = dataloader.dataset
     
-    def __init__(self, num_images, choose_randomly=False):
-        self.num_images=num_images
-        self.choose_randomly=choose_randomly
-        
-    def __call__(self, model: torch.nn.Module, 
-                 train_dl: DataLoader, val_dl: DataLoader, device):
-        
-        ds = val_dl.dataset
-        
-        preprocessor = Preprocessor(use_augmentations=False)
-        raw_preprocessor = Preprocessor(use_augmentations=False, equalize_hist=False, to_tensor=False)
+    if choose_randomly:
+        choices = np.random.choice(range(len(ds)), num_images)
+    else:
+        choices = range(num_images)
 
-        ds.preprocess = preprocessor
+    preprocessor = Preprocessor(use_augmentations=False)
+    raw_preprocessor = Preprocessor(use_augmentations=False, equalize_hist=False, to_tensor=False)
 
-        if self.choose_randomly:
-            choices = np.random.choice(range(len(ds)), self.num_images)
-        else:
-            choices = range(self.num_images)
-        
-        # get batch for inference
-        batch = torch.utils.data._utils.collate.default_collate(
-            [ds[choice] for choice in choices]
-        )
+    # get batch for inference
+    batch = torch.utils.data._utils.collate.default_collate(
+        [ds[choice] for choice in choices]
+    )
 
-        # get raw batch to show unprocessed mri image
-        ds.preprocess = raw_preprocessor
-        
-        raw_batch = torch.utils.data._utils.collate.default_collate(
-            [ds[choice] for choice in choices]
-        )
+    # get raw batch to show unprocessed mri image
+    ds.preprocess = raw_preprocessor
+    
+    raw_batch = torch.utils.data._utils.collate.default_collate(
+        [ds[choice] for choice in choices]
+    )
 
-        mris, masks = batch
-        raw_mris, _ = raw_batch
-        
+    mris, masks = batch
+    raw_mris, _ = raw_batch
+    
+    with torch.no_grad():
         logits = model(mris.to(device))
-        pred_masks = torch.argmax(logits, dim=1)
+
+    pred_masks = torch.argmax(logits, dim=1)
+    
+    images = []
+    for raw_mri, pred_mask, mask in zip(raw_mris, pred_masks, masks):
         
-        images = []
-        for raw_mri, pred_mask, mask in zip(raw_mris, pred_masks, masks):
-            
-            raw_mri = raw_mri.cpu().detach().numpy()
-            pred_mask = pred_mask.cpu().detach().numpy()
-            mask = mask.cpu().detach().numpy()
-            
-            images.append(wb_mask(raw_mri, pred_mask, mask))
+        raw_mri = raw_mri.cpu().detach().numpy()
+        pred_mask = pred_mask.cpu().detach().numpy()
+        mask = mask.cpu().detach().numpy()
         
-        wandb.log({'predictions': images})
-        
+        images.append(wb_mask(raw_mri, pred_mask, mask))
+
+    wandb.log({name: images})
+
