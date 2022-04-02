ff97058f0024eaee51e56cb0611fd41a2ce2bbf3
Paul Wilson <pfrwilson@gmail.com>
2022-04-02 14:04:47 -0400

    update training loop

--- src/train.py
+++ src/train.py
@@ -8,11 +8,11 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import _LRScheduler
 import torch
 from typing import Optional
-from torch.nn.functional import cross_entropy
+from .early_stopping import EarlyStopping
+from .logging import log_images_with_masks
 
 
-
-def train_epoch(epoch_idx: int, model: nn.Module, metrics: nn.Module,
+def train_epoch(epoch_idx: int, model: nn.Module, criterion, metrics: nn.Module,
           optim: Optimizer, dataloader: DataLoader, device,
           accumulate_grad_batches=1) -> dict:
     
@@ -28,7 +28,7 @@ def train_epoch(epoch_idx: int, model: nn.Module, metrics: nn.Module,
             
             logits = model(image)
             
-            loss = cross_entropy(logits, mask)
+            loss = criterion(logits, mask)
             loss.backward()
 
             if i % accumulate_grad_batches == 0: 
@@ -45,7 +45,7 @@ def train_epoch(epoch_idx: int, model: nn.Module, metrics: nn.Module,
     return history
         
         
-def eval_epoch(model: nn.Module, metrics: nn.Module,
+def eval_epoch(model: nn.Module, metrics: nn.Module, criterion, 
                dataloader: DataLoader, device) -> dict:
     
     model = model.eval().to(device)
@@ -53,7 +53,7 @@ def eval_epoch(model: nn.Module, metrics: nn.Module,
     
     with torch.no_grad():
         with tqdm(dataloader, desc=f'Evaluating') as pbar:
-            for (i, batch) in pbar:
+            for i, batch in enumerate(pbar):
                 
                 image, mask = batch
                 image = image.to(device)
@@ -61,7 +61,7 @@ def eval_epoch(model: nn.Module, metrics: nn.Module,
                 
                 logits = model(image)
                 
-                loss = cross_entropy(logits, mask)
+                loss = criterion(logits, mask)
                 if i % 10 == 0: 
                     pbar.set_postfix({'loss': loss.item()})
                 
@@ -72,24 +72,30 @@ def eval_epoch(model: nn.Module, metrics: nn.Module,
     return history
 
     
-def train(num_epochs, model: nn.Module, 
+def train(num_epochs, 
+          model: nn.Module, 
+          criterion, 
           train_metrics: nn.Module,
           eval_metrics: nn.Module, 
+          test_metrics: nn.Module,
           optim: Optimizer, 
           train_dataloader: DataLoader, 
           val_dataloader: DataLoader, 
+          test_dataloader: DataLoader, 
           device: torch.DeviceObjType,
           accumulate_grad_batches=1, 
           log_fn=None, 
+          log_images=True,
           scheduler: Optional[_LRScheduler]=None, 
-          epoch_end_callback=None,
+          early_stopping = None
           ):
-    
+
     for epoch in range(num_epochs):
         
-        history = train_epoch(
+        train_history = train_epoch(
             epoch, 
             model, 
+            criterion, 
             train_metrics, 
             optim, 
             train_dataloader, 
@@ -98,22 +104,39 @@ def train(num_epochs, model: nn.Module,
         )
 
         if log_fn: 
-            log_fn({f'train/{k}': v for k, v in history.items()})
+            log_fn({f'train/{k}': v for k, v in train_history.items()})
         
-        history = eval_epoch(
+        val_history = eval_epoch(
             model, 
             eval_metrics, 
+            criterion, 
             val_dataloader, 
             device
         )
         
         if log_fn: 
-            log_fn({f'val/{k}': v for k, v in history.items()})
+            log_fn({f'val/{k}': v for k, v in val_history.items()})
         
-        if epoch_end_callback:
-            epoch_end_callback(model, train_dataloader, val_dataloader, device)
-        
+        if log_images:
+            log_images_with_masks('val/images', model, val_dataloader, 4, True, device)
+
+        if early_stopping: 
+            early_stopping(val_history['loss'], model)
+
         if scheduler:
             scheduler.step()
-            log_fn({'lr', scheduler.get_last_lr()[0]})
+            if log_fn:     
+                log_fn({'lr': scheduler.get_last_lr()[0]})
     
+        if log_fn: 
+            log_fn({'epoch': epoch})
+
+        if early_stopping and early_stopping.early_stop: 
+            break
+
+    test_history = eval_epoch(model, test_metrics, criterion, test_dataloader, device)
+    if log_fn: 
+        log_fn({f'test/{k}': v for k, v in test_history.items()})
+
+    if log_images:
+        log_images_with_masks('test/images', model, test_dataloader, 12, True, device)
